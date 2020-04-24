c304934fe3174f6e1d841e5b0871d3b3b3b3c6e6
Kaustix <Kiernan.Lamarche@gmail.com>
2020-04-24 10:13:17 -0600

    cleaned up dev dependencies

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const app = express();
 
 app.use(express.static(`./dist/${project.name}`))
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
 app.get('*', function(req, res) {
   res.sendFile('index.html', { root: `dist/${project.name}`})
 })
